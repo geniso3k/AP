9297a747e41d66ea4fb99553dd111b7581b32598
Genis GB. BYTYQI <genis@sio.local>
2024-09-12 09:36:25 +0200

    Amélioration du JS, quand on reste appuyé ça devient jaune sur plusieurs cases

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function cliquer(un){
-
-    let div = document.getElementById(un);
-    alert(div);
-    div.addEventListener("mousedown", function(){
-        div.style.backgroundColor="yellow";
-    })
-
+function cliquer(id) {
+    let div = document.getElementById(id);
+    
+    // Changer la couleur au moment où l'on clique (mousedown)
+    div.addEventListener("mousedown", function() {
+        div.style.backgroundColor = "yellow";
+        
+        // Attacher l'événement "mouseup" au document pour détecter quand on relâche la souris
+        document.addEventListener("mouseup", function() {
+            div.style.backgroundColor = "white";
+        }, { once: true });  // Utilisation de { once: true } pour s'assurer que cet événement est déclenché une seule fois
+    });
 }
