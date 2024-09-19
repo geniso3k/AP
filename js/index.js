94da169474660c2597b5e2561f50e7c7f10ed3f4
Genis GB. BYTYQI <genis@sio.local>
2024-09-19 15:20:36 +0200

    Ajout classe calendrier

--- js/index.js
+++ js/index.js
@@ -3,22 +3,27 @@ let dayColumns = document.getElementsByClassName("day-column");
 
 // Ajoute un écouteur d'événements pour chaque colonne de jour
 Array.from(dayColumns).forEach(column => {
-    column.addEventListener("click", (e) => {
+    column.addEventListener("click", function(event){
         column.innerHTML = "<div class='event-slot'>esffs</div>";
     });
 });
 
 let retourneJour = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"];
 
-function AfficheCalendrier( idPlanning, jour) //Pour ajouter la date à coté de jeudi vendredi etc
-{
-    let planning = document.getElementById( idPlanning );
-    let entetesJour = document.getElementsByClassName("PlEntetesJour");
+function afficheCalendrier() //Pour ajouter la date à coté de jeudi vendredi etc
+{   
 
+    semainePro = new Date();
+    let lesDates = document.getElementsByClassName("day-header");
+    for(let uneDate of lesDates){
+            uneDate.innerText = "Jour "+i;
+            i++;
+    }
+/*
     for (let i=0; entetesJour.length; i++){
         let d = jour.getDay();
     }
-    return(new Date("y/m/"+d));
+    return(new Date("y/m/"+d));*/
 }
 
 
